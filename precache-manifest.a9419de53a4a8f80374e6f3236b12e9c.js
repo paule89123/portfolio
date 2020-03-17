c3cdd9234a9cf2714be495a875f91a54b953bda2
Paul Evans <paulevans89@gmail.com>
2020-03-17 02:32:10 +0000

    Updates

---
+++ precache-manifest.a9419de53a4a8f80374e6f3236b12e9c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33df2bcd33b30908b083c09e785f750",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "aa535fca65645e0779c8",
+    "url": "/portfolio/static/css/main.98cd3fbd.chunk.css"
+  },
+  {
+    "revision": "3f71eaad5253fb6969e9",
+    "url": "/portfolio/static/js/2.f5c4f9b9.chunk.js"
+  },
+  {
+    "revision": "e9a45d9472c063c8a1c023664dfc3c9f",
+    "url": "/portfolio/static/js/2.f5c4f9b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa535fca65645e0779c8",
+    "url": "/portfolio/static/js/main.5a4488fe.chunk.js"
+  },
+  {
+    "revision": "bdca0bbe2605fb6d5426",
+    "url": "/portfolio/static/js/runtime-main.85230087.js"
+  },
+  {
+    "revision": "974bb24bfecde47740baf684e401b72e",
+    "url": "/portfolio/static/media/CV.974bb24b.svg"
+  },
+  {
+    "revision": "c07c916c55ef23e1f0a0dbcb10b9aaae",
+    "url": "/portfolio/static/media/Karla-Bold.c07c916c.ttf"
+  },
+  {
+    "revision": "ebf98a26db2e0ccb66cd970557a51248",
+    "url": "/portfolio/static/media/Karla-BoldItalic.ebf98a26.ttf"
+  },
+  {
+    "revision": "1b55fee684d61bfeaa762684931b1bc9",
+    "url": "/portfolio/static/media/Karla-Regular.1b55fee6.ttf"
+  },
+  {
+    "revision": "36153d464bc7b82f3b22dbccc739624f",
+    "url": "/portfolio/static/media/TiemposHeadline-Semibold.36153d46.otf"
+  },
+  {
+    "revision": "191fc87c1797f5a75bb6a73301994c51",
+    "url": "/portfolio/static/media/about-placeholder.191fc87c.jpg"
+  },
+  {
+    "revision": "1a319912a8e144099e85839caa581834",
+    "url": "/portfolio/static/media/calculator-large.1a319912.jpg"
+  },
+  {
+    "revision": "66e806ee9b233c45db04be28029f046c",
+    "url": "/portfolio/static/media/calculator.66e806ee.png"
+  },
+  {
+    "revision": "3678b31c1d244c38265b1484b11bdb64",
+    "url": "/portfolio/static/media/codewars.3678b31c.svg"
+  },
+  {
+    "revision": "826230bb480d845a2d1fdb0cec9cab1d",
+    "url": "/portfolio/static/media/coverful-cropped.826230bb.mp4"
+  },
+  {
+    "revision": "5d560a0233749ff68b10c4c2409c8e64",
+    "url": "/portfolio/static/media/coverful.5d560a02.png"
+  },
+  {
+    "revision": "192a6620a687a9e7e51c0037973a1547",
+    "url": "/portfolio/static/media/github.192a6620.svg"
+  },
+  {
+    "revision": "9184231e92dc50a6a3710fb37d45640b",
+    "url": "/portfolio/static/media/joanne-site-cropped.9184231e.mp4"
+  },
+  {
+    "revision": "bbc8d09ff892b180290217b057319f2a",
+    "url": "/portfolio/static/media/joanne.bbc8d09f.png"
+  },
+  {
+    "revision": "5b238434e2862c877f08572b96c0ef7b",
+    "url": "/portfolio/static/media/linkedin.5b238434.svg"
+  },
+  {
+    "revision": "0b0a036b601958d0b7c7963e4bbf53d0",
+    "url": "/portfolio/static/media/me-fade.0b0a036b.png"
+  },
+  {
+    "revision": "c1162375c09d9bbda4e2aa3c15f14eee",
+    "url": "/portfolio/static/media/me.c1162375.png"
+  },
+  {
+    "revision": "a450cc103b020589bf97577c53c009d7",
+    "url": "/portfolio/static/media/menu.a450cc10.svg"
+  },
+  {
+    "revision": "2d6d33089b83f386005262c69fb60187",
+    "url": "/portfolio/static/media/reminders-cropped.2d6d3308.mp4"
+  },
+  {
+    "revision": "842126b8a0fcf7f4fffbc7724d7cadf5",
+    "url": "/portfolio/static/media/reminders.842126b8.png"
+  }
+]);
