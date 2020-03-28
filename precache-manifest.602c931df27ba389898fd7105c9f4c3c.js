7376f227fd566a96e4d3f7bd21dbe1ce8463e96c
Paul Evans <paulevans89@gmail.com>
2020-03-28 16:39:57 +0000

    Updates

---
+++ precache-manifest.602c931df27ba389898fd7105c9f4c3c.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca694c9504e1828ecd651a1b0062ed2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01998eb55aa44937c765",
+    "url": "/static/css/main.2cafcbeb.chunk.css"
+  },
+  {
+    "revision": "937131dc2f541e9f948b",
+    "url": "/static/js/2.2a115c89.chunk.js"
+  },
+  {
+    "revision": "e9a45d9472c063c8a1c023664dfc3c9f",
+    "url": "/static/js/2.2a115c89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01998eb55aa44937c765",
+    "url": "/static/js/main.96d7a68e.chunk.js"
+  },
+  {
+    "revision": "4b1dfec29c9223670076",
+    "url": "/static/js/runtime-main.e6f5c655.js"
+  },
+  {
+    "revision": "75da132a39899e76e59fdecee4a3dd7f",
+    "url": "/static/media/CV.75da132a.svg"
+  },
+  {
+    "revision": "c07c916c55ef23e1f0a0dbcb10b9aaae",
+    "url": "/static/media/Karla-Bold.c07c916c.ttf"
+  },
+  {
+    "revision": "ebf98a26db2e0ccb66cd970557a51248",
+    "url": "/static/media/Karla-BoldItalic.ebf98a26.ttf"
+  },
+  {
+    "revision": "1b55fee684d61bfeaa762684931b1bc9",
+    "url": "/static/media/Karla-Regular.1b55fee6.ttf"
+  },
+  {
+    "revision": "36153d464bc7b82f3b22dbccc739624f",
+    "url": "/static/media/TiemposHeadline-Semibold.36153d46.otf"
+  },
+  {
+    "revision": "30fdc9d9413a52ab8ef343d4bcf22857",
+    "url": "/static/media/about2.30fdc9d9.jpg"
+  },
+  {
+    "revision": "1a319912a8e144099e85839caa581834",
+    "url": "/static/media/calculator.1a319912.jpg"
+  },
+  {
+    "revision": "3678b31c1d244c38265b1484b11bdb64",
+    "url": "/static/media/codewars.3678b31c.svg"
+  },
+  {
+    "revision": "826230bb480d845a2d1fdb0cec9cab1d",
+    "url": "/static/media/coverful-cropped.826230bb.mp4"
+  },
+  {
+    "revision": "44f99b3b7a2d3a3f8d2680736da364a0",
+    "url": "/static/media/coverful.44f99b3b.gif"
+  },
+  {
+    "revision": "c4553c4eeec2d5cabf99c7fafa3156e8",
+    "url": "/static/media/coverful.c4553c4e.png"
+  },
+  {
+    "revision": "192a6620a687a9e7e51c0037973a1547",
+    "url": "/static/media/github.192a6620.svg"
+  },
+  {
+    "revision": "9184231e92dc50a6a3710fb37d45640b",
+    "url": "/static/media/joanne-site-cropped.9184231e.mp4"
+  },
+  {
+    "revision": "679f996eda506ef738e0f52440d13e88",
+    "url": "/static/media/joanne-site.679f996e.gif"
+  },
+  {
+    "revision": "aa3392b052f07355d502f9c9c63e9487",
+    "url": "/static/media/joanne.aa3392b0.png"
+  },
+  {
+    "revision": "5b238434e2862c877f08572b96c0ef7b",
+    "url": "/static/media/linkedin.5b238434.svg"
+  },
+  {
+    "revision": "bfe8a6b50ba5128064b2294299fa8cc4",
+    "url": "/static/media/mail.bfe8a6b5.svg"
+  },
+  {
+    "revision": "0b0a036b601958d0b7c7963e4bbf53d0",
+    "url": "/static/media/me-fade.0b0a036b.png"
+  },
+  {
+    "revision": "c1162375c09d9bbda4e2aa3c15f14eee",
+    "url": "/static/media/me.c1162375.png"
+  },
+  {
+    "revision": "a450cc103b020589bf97577c53c009d7",
+    "url": "/static/media/menu.a450cc10.svg"
+  },
+  {
+    "revision": "2d6d33089b83f386005262c69fb60187",
+    "url": "/static/media/reminders-cropped.2d6d3308.mp4"
+  },
+  {
+    "revision": "842126b8a0fcf7f4fffbc7724d7cadf5",
+    "url": "/static/media/reminders.842126b8.png"
+  },
+  {
+    "revision": "c9f5e219ee9f61422f3ce11983b81908",
+    "url": "/static/media/reminders.c9f5e219.gif"
+  },
+  {
+    "revision": "5b1e6bb0333658d3269461e524987c73",
+    "url": "/static/media/spotify-playlist-maker-cropped.5b1e6bb0.mp4"
+  },
+  {
+    "revision": "770da793214e3f90d0ebca3878ffe2e8",
+    "url": "/static/media/spotify-playlist-maker.770da793.gif"
+  },
+  {
+    "revision": "829bf4b545a2674f8260c232086c8415",
+    "url": "/static/media/spotify.829bf4b5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.729053b70d3f17ba507a429ab9d38411.js"
+  "/precache-manifest.602c931df27ba389898fd7105c9f4c3c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
